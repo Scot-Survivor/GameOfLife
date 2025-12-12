610c89a1286af9982df5769aefe3114fb5cda7d6
Scot-Survivor <40865296+Scot-Survivor@users.noreply.github.com>
2025-12-12 19:25:26 +0000

    fix: nix shell

--- shell.nix
+++ shell.nix
@@ -11,5 +11,10 @@ pkgs.mkShell {
     xorg.libXext
     glfw
     glew
-  ];
+    libxcb
+    pkg-config
+  ] ++ lib.mapAttrsToList (attr: value: if (lib.hasPrefix "lib" attr && attr != "libAppleWM") then value else null) xorg;
+shellHook = ''
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.xorg.libXext}/lib:${pkgs.xorg.libX11}/lib
+  '';
 }
